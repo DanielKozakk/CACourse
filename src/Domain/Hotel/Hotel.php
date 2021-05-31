ef9c3d89343e04039aaaffbf4162623948133f4f
Daniel Kozak <kozak2321@gmail.com>
2021-05-31 17:44:56 +0200

    Nearly ended exercise

---
+++ src/Domain/Hotel/Hotel.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Domain\Hotel;
+
+
+use App\Domain\Apartment\Address;
+
+class Hotel
+{
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var Address
+     */
+    private $address;
+}
