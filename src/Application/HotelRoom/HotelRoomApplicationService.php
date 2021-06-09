a9d56c770c128a175013f4d4ab485febd0cb8073
Daniel Kozak <kozak2321@gmail.com>
2021-06-09 09:34:45 +0200

    refactor

--- src/Application/HotelRoom/HotelRoomApplicationService.php
+++ src/Application/HotelRoom/HotelRoomApplicationService.php
@@ -48,16 +48,12 @@ class HotelRoomApplicationService
         array $spacesDefinition,
         string $description,
         string $hotel
-    )
-    {
+    ){
         $hotelRoom =  (new HotelRoomFactory())->create($number, $spacesDefinition, $description, $this->hotelRepository->findById($hotel));
-
         $this->hotelRoomRepository->save($hotelRoom);
-
     }
 
-    public function book(int $id, string $tenantId, \DateTime $startDate, \DateTime $endDate)
-    {
+    public function book(int $id, string $tenantId, \DateTime $startDate, \DateTime $endDate){
         $period = new Period ($startDate, $endDate);
         $hotelRoom = $this->hotelRoomRepository->findById($id);
         $hotelRoom->book($tenantId,$period, $this->eventChannel);
