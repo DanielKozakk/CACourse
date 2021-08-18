e0cff0f9bbde51a4d5cf2a1290b767abd1e69c51
Daniel Kozak <kozak2321@gmail.com>
2021-08-18 08:24:49 +0200

    dataprovider into test

--- tests/Domain/Apartment/PeriodTest.php
+++ tests/Domain/Apartment/PeriodTest.php
@@ -9,38 +9,55 @@ use PHPUnit\Framework\TestCase;
 
 class PeriodTest extends TestCase
 {
+    /**
+     * @param string $expectedStartDate
+     * @param string $expectedEndDate
+     * @param int $expectedCount
+     * @param array $expectedArrayOfDays
+     * @throws \Exception
+     * @dataProvider dataProvider
+     */
 
-    public function testShouldPeriodContainsTwoDays():void{
-        $start = new DateTime('2000-01-01');
-        $end = new DateTime('2000-01-02');
+    public function testShouldPeriodContains(string $expectedStartDate, string $expectedEndDate, int $expectedCount, array $expectedArrayOfDays): void
+    {
+        $start = new DateTime($expectedStartDate);
+        $end = new DateTime($expectedEndDate);
 
         $period = new Period(clone($start), clone($end));
-
         $dateTimeArray = $period->asDateTimeArray();
 
-        $this->assertCount(2, $dateTimeArray);
-        $this->assertEquals($start, $dateTimeArray[0]);
-        $this->assertEquals($end, $dateTimeArray[1]);
+        $this->assertCount($expectedCount, $dateTimeArray);
+        $this->assertEqualsCanonicalizing(
+                $expectedArrayOfDays,
+                $dateTimeArray
+        );
     }
 
-    public function testShouldPeriodContainsSevenDays():void{
-        $start = new DateTime('2021-08-08');
-        $end = new DateTime('2021-08-14');
+    public function dataProvider()
+    {
+        $expectedDate = '2000-01-01';
+        $expectedEnd = '2000-01-02';
+        $expectedCount = 2;
+        $expectedArray = [
+            new DateTime('2000-01-01'),
+            new DateTime('2000-01-02')
+        ];
+        yield [$expectedDate, $expectedEnd, $expectedCount, $expectedArray];
 
-        $period = new Period(clone($start), clone($end));
-        $dateTimeArray = $period->asDateTimeArray();
-
-        $this->assertCount(7, $dateTimeArray);
-        $this->assertEqualsCanonicalizing(
-            [new DateTime('2021-08-08'),
+        $expectedDate = '2021-08-08';
+        $expectedEnd = '2021-08-14';
+        $expectedCount = 7;
+        $expectedArray = [
+            new DateTime('2021-08-08'),
             new DateTime('2021-08-09'),
             new DateTime('2021-08-10'),
             new DateTime('2021-08-11'),
             new DateTime('2021-08-12'),
             new DateTime('2021-08-13'),
-            new DateTime('2021-08-14')],
-            $dateTimeArray
-        );
+            new DateTime('2021-08-14'),
+        ];
+        yield [$expectedDate, $expectedEnd, $expectedCount, $expectedArray];
+
     }
 
 }
