ab30cacb162d6935a2c620a5b6474c9df3b2de5a
Daniel Kozak <kozak2321@gmail.com>
2021-09-10 10:42:31 +0200

    create of entities

--- src/Domain/Hotel/HotelBookingHistory/HotelRoomBooking.php
+++ src/Domain/Hotel/HotelBookingHistory/HotelRoomBooking.php
@@ -6,9 +6,12 @@ use DateTime;
 
 class HotelRoomBooking
 {
-    public static function start(string $hotelRoomId, DateTime $eventCreationDateTime, string $tenantId, array $days){
 
 
+    public static function start(string $hotelRoomId, DateTime $eventCreationDateTime, string $tenantId, array $days) : HotelRoomBooking{
+
+
+        return new HotelRoomBooking();
     }
 
 }
