ba9b0c44f2d9137b4874426e77e736853f7589d4
Daniel Kozak <kozak2321@gmail.com>
2021-09-16 09:11:45 +0200

    introduce new repository for read model

--- src/Query/Apartment/QueryApartmentRepository.php
+++ src/Query/Apartment/QueryApartmentRepository.php
@@ -5,11 +5,21 @@ namespace Query\Apartment;
 class QueryApartmentRepository
 {
 
+    private DoctrineQueryApartmentRepository $springQueryApartmentRepository;
+
+    /**
+     * @param DoctrineQueryApartmentRepository $springQueryApartmentRepository
+     */
+    public function __construct(DoctrineQueryApartmentRepository $springQueryApartmentRepository)
+    {
+        $this->springQueryApartmentRepository = $springQueryApartmentRepository;
+    }
+
+
     /**
      * @return array<ApartmentReadModel>
      */
     public function findAll() : array{
-
-
+        return $this->springQueryApartmentRepository->findAll();
     }
 }
