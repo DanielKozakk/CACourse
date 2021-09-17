e046ad05aa145c4a99d58301079d51466fe38df1
Daniel Kozak <kozak2321@gmail.com>
2021-09-17 07:31:13 +0200

    apartment booking read model

--- src/Query/Apartment/ApartmentBookingReadModel.php
+++ src/Query/Apartment/ApartmentBookingReadModel.php
@@ -2,7 +2,7 @@
 
 namespace Query\Apartment;
 
-// ta sama tabela co apartmentBooking
+//TODO ta sama tabela co apartmentBooking
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -21,7 +21,7 @@ class ApartmentBookingReadModel
      * @var DateTime
      * @ORM\Column(type="datetime")
      */
-    private DateTime $bookingCreationDateTime;
+    private DateTime $bookingCreation;
     /**
      * @var string
      * @ORM\Column(type="string")
@@ -43,6 +43,86 @@ class ApartmentBookingReadModel
      * @ORM\Column(type="datetime")
      */
     private DateTime $endDate;
+
+    /**
+     *  @ORM\Column(type="string")
+     */
+    private string $bookingStep;
+
+    /**
+     * @param DateTime $bookingCreation
+     * @param string $ownerId
+     * @param string $tenantId
+     * @param DateTime $startDate
+     * @param DateTime $endDate
+     * @param string $bookingStep
+     */
+    public function __construct(DateTime $bookingCreation, string $ownerId, string $tenantId, DateTime $startDate, DateTime $endDate, string $bookingStep)
+    {
+        $this->bookingCreation = $bookingCreation;
+        $this->ownerId = $ownerId;
+        $this->tenantId = $tenantId;
+        $this->startDate = $startDate;
+        $this->endDate = $endDate;
+        $this->bookingStep = $bookingStep;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getBookingCreation(): DateTime
+    {
+        return $this->bookingCreation;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOwnerId(): string
+    {
+        return $this->ownerId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTenantId(): string
+    {
+        return $this->tenantId;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getStartDate(): DateTime
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getEndDate(): DateTime
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBookingStep(): string
+    {
+        return $this->bookingStep;
+    }
+
 
 
 }
