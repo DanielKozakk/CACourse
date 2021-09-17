70b9de653c7b2e2dfee5c6bddd25019df39da47e
Daniel Kozak <kozak2321@gmail.com>
2021-09-17 07:31:43 +0200

    apartment details

--- src/Query/Apartment/ApartmentDetails.php
+++ src/Query/Apartment/ApartmentDetails.php
@@ -10,5 +10,30 @@ class ApartmentDetails
     private ApartmentReadModel $apartmentReadModel;
     private ApartmentBookingHistoryReadModel $apartmentBookingHistoryReadModel;
 
+    /**
+     * @param ApartmentReadModel $apartmentReadModel
+     * @param ApartmentBookingHistoryReadModel $apartmentBookingHistoryReadModel
+     */
+    public function __construct(ApartmentReadModel $apartmentReadModel, ApartmentBookingHistoryReadModel $apartmentBookingHistoryReadModel)
+    {
+        $this->apartmentReadModel = $apartmentReadModel;
+        $this->apartmentBookingHistoryReadModel = $apartmentBookingHistoryReadModel;
+    }
+
+    /**
+     * @return ApartmentReadModel
+     */
+    public function getApartmentReadModel(): ApartmentReadModel
+    {
+        return $this->apartmentReadModel;
+    }
+
+    /**
+     * @return ApartmentBookingHistoryReadModel
+     */
+    public function getApartmentBookingHistoryReadModel(): ApartmentBookingHistoryReadModel
+    {
+        return $this->apartmentBookingHistoryReadModel;
+    }
 
 }
