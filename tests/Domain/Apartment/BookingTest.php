8c27314e8811502df11903ee3a43a14a2a027587
Daniel Kozak <kozak2321@gmail.com>
2021-09-22 13:00:56 +0200

    new assertoin and two test

--- tests/Domain/Apartment/BookingTest.php
+++ tests/Domain/Apartment/BookingTest.php
@@ -4,20 +4,36 @@ namespace Domain\Apartment;
 
 use DateTime;
 use PHPUnit\Framework\TestCase;
+use ReflectionException;
+
+require_once ('BookingAssertion.php');
 
 class BookingTest extends TestCase
 {
 
-    public function testShouldCreateBookingForApartment(){
+    /**
+     * @throws ReflectionException
+     */
+    public function testShouldCreateBookingApartmentWithAllRequiredFields (){
 
-//        $rentalPlaceId = '990';
-//        $tenantId=  '124';
-//        $startDate = DateTime::createFromFormat('Y-m-d','')
-//
-//        $booking = Booking::bookApartment($rentalPlaceId, $tenantId, new Period($startDate, $endDate));
-    }
+        $rentalPlaceId = '9634';
+        $tenantId = '12511';
 
-    public function testShouldCreateBookingForHotelRoom(){
+        $periodStart=  DateTime::createFromFormat('d-m-Y', '09-05-1995');
+        $periodEnd=  DateTime::createFromFormat('d-m-Y', '09-05-1995');
+        $period = new Period($periodStart, $periodEnd);
+
+        $actual = Booking::bookApartment($rentalPlaceId, $tenantId, $period);
+
+        $expectedDays = $period->asDateTimeArray();
+
+        BookingAssertion::assert($actual)
+            ->isOpen()
+            ->hasRentalTypeEqualsTo(RentalType::APARTMENT);
+
+//            ->hasRentalPlaceIdEqualsTo($rentalPlaceId)
+//            ->hasTenantIdEqualsTo($tenantId)
+//            ->hasDaysEqualsTo($expectedDays);
 
     }
 }
