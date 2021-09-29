906045144497fb19859043c872df652354091121
Daniel Kozak <kozak2321@gmail.com>
2021-09-29 09:57:53 +0200

    repo for apartment

--- src/Domain/Apartment/ApartmentRepository.php
+++ src/Domain/Apartment/ApartmentRepository.php
@@ -1,11 +1,11 @@
 <?php
-//
-//namespace Domain\Apartment;
-//
-//interface ApartmentRepository
-//{
-//    public function save(Apartment $apartment);
-//
-//    public function findById(string $apartmentId);
-//
-//}
+
+namespace Domain\Apartment;
+
+interface ApartmentRepository
+{
+    public function save(Apartment $apartment);
+
+    public function findById(string $apartmentId);
+
+}
