f2bf4e9527ec1cbe4122848967fdd1fa44bfd8a7
Daniel Kozak <kozak2321@gmail.com>
2021-10-05 13:10:45 +0200

    hotel room read model uncomment

--- src/Query/Hotel/HotelRoom/SqlDoctrineQueryHotelRoomRepository.php
+++ src/Query/Hotel/HotelRoom/SqlDoctrineQueryHotelRoomRepository.php
@@ -1,38 +1,38 @@
 <?php
-//
-//namespace Query\Hotel\HotelRoom;
-//
-//use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-//use Doctrine\ORM\EntityManagerInterface;
-//use Doctrine\Persistence\ManagerRegistry;
-//
-//
-//
-//
-///**
-// * @method HotelRoomReadModel|null find($id, $lockMode = null, $lockVersion = null)
-// * @method HotelRoomReadModel|null findOneBy(array $criteria, array $orderBy = null)
-// * @method HotelRoomReadModel[]    findAll()
-// * @method HotelRoomReadModel[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
-// */
-//class SqlDoctrineQueryHotelRoomRepository extends ServiceEntityRepository
-//{
-//
-//    private EntityManagerInterface $entityManager;
-//
-//    public function __construct(ManagerRegistry $registry, EntityManagerInterface $entityManager)
-//    {
-//        parent::__construct($registry, HotelRoomReadModel::class);
-//        $this->entityManager = $entityManager;
-//    }
-//
-//    /**
-//     * @param string $hotelId
-//     * @return HotelRoomReadModel[]
-//     */
-//    public function findByHotelId(string $hotelId): array
-//    {
-//        return $this->findBy(['hotelId' => $hotelId]);
-//    }
-//
-//}
+
+namespace Query\Hotel\HotelRoom;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
+
+
+
+/**
+ * @method HotelRoomReadModel|null find($id, $lockMode = null, $lockVersion = null)
+ * @method HotelRoomReadModel|null findOneBy(array $criteria, array $orderBy = null)
+ * @method HotelRoomReadModel[]    findAll()
+ * @method HotelRoomReadModel[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SqlDoctrineQueryHotelRoomRepository extends ServiceEntityRepository
+{
+
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(ManagerRegistry $registry, EntityManagerInterface $entityManager)
+    {
+        parent::__construct($registry, HotelRoomReadModel::class);
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param string $hotelId
+     * @return HotelRoomReadModel[]
+     */
+    public function findByHotelId(string $hotelId): array
+    {
+        return $this->findBy(['hotelId' => $hotelId]);
+    }
+
+}
