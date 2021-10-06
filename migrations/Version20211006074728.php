6090f1a76623bfdc5156ac0b780d0389e9f8f830
Daniel Kozak <kozak2321@gmail.com>
2021-10-06 09:48:32 +0200

    booking entity migration

---
+++ migrations/Version20211006074728.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211006074728 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE booking (id INT AUTO_INCREMENT NOT NULL, rental_place_id VARCHAR(255) NOT NULL, tenant_id VARCHAR(255) NOT NULL, days LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', rental_type_state VARCHAR(255) NOT NULL, booking_status_state VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE booking');
+    }
+}
--- src/Domain/Apartment/Booking.php
+++ src/Domain/Apartment/Booking.php
@@ -1,59 +1,81 @@
 <?php
-//
-//namespace Domain\Apartment;
-//
-//use DateTime;
-//use Doctrine\ORM\Mapping as ORM;
+
+namespace Domain\Apartment;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
 //use Domain\EventChannel\EventChannel;
-//
-//
-////TODO: entity
-//
-///**
-// * @ ORM\Entity
-// */
-//class Booking
-//{
-//
-//    /**
-//     * @var string
-//     * @ORM\Id
-//     */
-//    private string $id;
-//    private RentalType $rentalType;
-//    private string $rentalPlaceId;
-//    private string $tenantId;
-//    private array $days;
-//    private BookingStatus $bookingStatus;
-//
-//    /**
-//     * @param RentalType $rentalType
-//     * @param string $rentalPlaceId
-//     * @param string $tenantId
-//     * @param array $days
-//     */
-//    private function __construct(RentalType $rentalType, string $rentalPlaceId, string $tenantId, array $days)
-//    {
-//        $this->rentalType = $rentalType;
-//        $this->rentalPlaceId = $rentalPlaceId;
-//        $this->tenantId = $tenantId;
-//        $this->days = $days;
-//        $this->bookingStatus = BookingStatus::open();
-//    }
-//
-//
-//    public static function bookApartment(string $rentalPlaceId, string $tenantId, Period $period): Booking
-//    {
-//        return new Booking(RentalType::apartmentRentalType(),$rentalPlaceId, $tenantId, $period->asDateTimeArray());
-//    }
-//    public static function bookHotelRoom(string $rentalSpaceId, string $tenantId, array $days): Booking
-//    {
-//        return new Booking(RentalType::hotelRoomRentalType(), $rentalSpaceId, $tenantId, $days);
-//    }
-//
-//    public function reject(){
-//        $this->bookingStatus = BookingStatus::rejected();
-//    }
+
+/**
+ * @ORM\Entity
+ */
+class Booking
+{
+
+    /**
+     * @var string
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private string $id;
+
+    /**
+     * @var RentalType
+     * @ORM\Embedded
+     */
+    private RentalType $rentalType;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private string $rentalPlaceId;
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private string $tenantId;
+
+    /**
+     * @var array
+     * @ORM\Column(type="array")
+     */
+    private array $days;
+    /**
+     *
+     * @ORM\Embedded
+     */
+    private BookingStatus $bookingStatus;
+
+    /**
+     * @param RentalType $rentalType
+     * @param string $rentalPlaceId
+     * @param string $tenantId
+     * @param array $days
+     */
+    private function __construct(RentalType $rentalType, string $rentalPlaceId, string $tenantId, array $days)
+    {
+        $this->rentalType = $rentalType;
+        $this->rentalPlaceId = $rentalPlaceId;
+        $this->tenantId = $tenantId;
+        $this->days = $days;
+        $this->bookingStatus = BookingStatus::open();
+    }
+
+
+    public static function bookApartment(string $rentalPlaceId, string $tenantId, Period $period): Booking
+    {
+        return new Booking(RentalType::apartmentRentalType(),$rentalPlaceId, $tenantId, $period->asDateTimeArray());
+    }
+    public static function bookHotelRoom(string $rentalSpaceId, string $tenantId, array $days): Booking
+    {
+        return new Booking(RentalType::hotelRoomRentalType(), $rentalSpaceId, $tenantId, $days);
+    }
+
+    public function reject(){
+        $this->bookingStatus = BookingStatus::rejected();
+    }
 //    public function accept(EventChannel $eventChannel){
 //        $this->bookingStatus = BookingStatus::accept();
 //        /**
@@ -64,5 +86,5 @@
 //        $eventChannel->publishBookingAcceptedEvent($bookingAcceptedEvent);
 //
 //    }
-//
-//}
+
+}
