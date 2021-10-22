3e5e07d87e6968b2ee2d43a8201ed467988edb38
Daniel Kozak <kozak2321@gmail.com>
2021-10-22 11:57:51 +0200

    another test

--- tests/Application/Apartment/ApartmentApplicationServiceTest.php
+++ tests/Application/Apartment/ApartmentApplicationServiceTest.php
@@ -3,13 +3,16 @@
 namespace Application\Apartment;
 
 use DataFixtures\ApartmentFixture;
+use DateTime;
 use Domain\Apartment\Apartment;
 use Domain\Apartment\ApartmentAssertion;
 use Domain\Apartment\ApartmentRepository;
 use Domain\Apartment\Booking;
 use Domain\Apartment\BookingAssertion;
 use Domain\Apartment\BookingRepository;
+use Domain\Apartment\RentalType;
 use Domain\EventChannel\EventChannel;
+use Infrastructure\Persistence\Doctrine\Apartment\DoctrineApartmentRepository;
 use Infrastructure\Persistence\Doctrine\Booking\DoctrineBookingRepository;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -22,6 +25,7 @@ class ApartmentApplicationServiceTest extends WebTestCase
     private EventChannel $eventChannel;
     private BookingRepository $bookingRepository;
 
+    private const APARTMENT_ID = ApartmentFixture::FIRST_TEST_APARTMENT['apartmentId'];
     private const OWNER_ID = ApartmentFixture::FIRST_TEST_APARTMENT['ownerId'];
     private const STREET = ApartmentFixture::FIRST_TEST_APARTMENT['street'];
     private const POSTAL_CODE = ApartmentFixture::FIRST_TEST_APARTMENT['postalCode'];
@@ -32,25 +36,33 @@ class ApartmentApplicationServiceTest extends WebTestCase
     private const DESCRIPTION = ApartmentFixture::FIRST_TEST_APARTMENT['description'];
     private const ROOMS_DEFINITION = ApartmentFixture::FIRST_TEST_APARTMENT['roomsDefinition'];
 
+    private const TENANT_ID = '3203';
+    private DateTime $bookingStart;
+    private DateTime $bookingEnd;
+
+
     public function __construct()
     {
         parent::__construct();
         self::bootKernel();
 
+        $this->bookingStart = new DateTime('2021-01-02');
+        $this->bookingEnd = new DateTime('2021-01-03');
+
         $this->apartmentRepository = $this->createMock(ApartmentRepository::class);
         $this->eventChannel = $this->createStub(EventChannel::class);
-        $this->bookingRepository = $this->getContainer()->get(DoctrineBookingRepository::class);
+        $this->bookingRepository = $this->createMock(DoctrineBookingRepository::class);
 
         $this->apartmentApplicationService = new ApartmentApplicationService($this->apartmentRepository, $this->eventChannel, $this->bookingRepository);
     }
 
-    public function testAddApartment()
+    public function testShouldCreateApartmentWithAllParameters()
     {
         $this->apartmentRepository->expects($this->once())->method('save')->with($this->callback(
-            function (Apartment $apartment){
+            function (Apartment $apartment) {
                 ApartmentAssertion::assert($apartment)
                     ->hasOwnerIdEqualsTo(self::OWNER_ID)
-                    ->hasAddressEqualsTo(self::STREET, self::POSTAL_CODE,self::HOUSE_NUMBER, self::APARTMENT_NUMBER, self::CITY, self::COUNTRY)
+                    ->hasAddressEqualsTo(self::STREET, self::POSTAL_CODE, self::HOUSE_NUMBER, self::APARTMENT_NUMBER, self::CITY, self::COUNTRY)
                     ->hasRoomsEqualsTo(self::ROOMS_DEFINITION);
                 return true;
             }
@@ -65,5 +77,33 @@ class ApartmentApplicationServiceTest extends WebTestCase
             self::COUNTRY,
             self::DESCRIPTION,
             self::ROOMS_DEFINITION);
+    }
+
+    private function givenApartmentWithId(int $id)
+    {
+        $apartment = $this->getContainer()->get(DoctrineApartmentRepository::class)->findById($id);
+        $this->apartmentRepository->method('findById')->willReturn($apartment);
+    }
+
+    public function testShouldBookApartmentWithAllParameters()
+    {
+        $this->givenApartmentWithId(self::APARTMENT_ID);
+
+        $this->bookingRepository->expects($this->once())->method('save')->with(
+            $this->callback(
+                function (Booking $booking){
+
+                    BookingAssertion::assert($booking)
+                        ->isOpen()
+                        ->hasTenantIdEqualsTo(self::TENANT_ID)
+                        ->hasRentalTypeEqualsTo(RentalType::apartmentRentalType()->getState())
+                        ->hasRentalPlaceIdEqualsTo(self::APARTMENT_ID)
+                        ->hasDaysEqualsTo([$this->bookingStart, $this->bookingEnd]);
+                    return true;
+                }
+            )
+        );
+
+        $this->apartmentApplicationService->book(self::APARTMENT_ID, self::TENANT_ID, $this->bookingStart, $this->bookingEnd);
     }
 }
