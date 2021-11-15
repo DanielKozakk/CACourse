e8064dbd2a266ca5a9f60238f437caededa77a69
Daniel Kozak <kozak2321@gmail.com>
2021-11-15 13:08:42 +0100

    apartment rest controller service yaml

--- src/Infrastructure/Rest/Api/Apartment/ApartmentRestController.php
+++ src/Infrastructure/Rest/Api/Apartment/ApartmentRestController.php
@@ -1,44 +1,56 @@
 <?php
 
 namespace Infrastructure\Rest\Api\Apartment;
-
 use Application\Apartment\ApartmentApplicationService;
-use Query\Apartment\ApartmentDetails;
+//use Query\Apartment\ApartmentDetails;
 use Query\Apartment\ApartmentReadModel;
-use Query\Apartment\QueryApartmentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-//TODO: https://api-platform.com/ - tutaj jest dokumentacja do tworzenia ładnego api do symfony.
-// Nie jestem pewien, czy tworzy się tam takie kontrollery - dostosuj funkcjonalność tej klasy do powyższego frameworka, gdy już będziesz chciał używac funkcjonalności.
-class ApartmentRestController
+//use Query\Apartment\QueryApartmentRepository;
+
+class ApartmentRestController extends AbstractController
 {
 
-    private ApartmentApplicationService $apartmentApplicationService;
-    private QueryApartmentRepository $queryApartmentRepository;
+//    private ApartmentApplicationService $apartmentApplicationService;
+//    private QueryApartmentRepository $queryApartmentRepository;
 
-    /**
-     * @param ApartmentApplicationService $apartmentApplicationService
-     * @param QueryApartmentRepository $queryApartmentRepository
-     */
-    public function __construct(ApartmentApplicationService $apartmentApplicationService, QueryApartmentRepository $queryApartmentRepository)
+//    /**
+//     * @param ApartmentApplicationService $apartmentApplicationService
+//     * @param QueryApartmentRepository $queryApartmentRepository
+//     */
+//    public function __construct(ApartmentApplicationService $apartmentApplicationService, QueryApartmentRepository $queryApartmentRepository)
+//    {
+//        $this->apartmentApplicationService = $apartmentApplicationService;
+//        $this->queryApartmentRepository = $queryApartmentRepository;
+//    }
+
+    #[Route('/apartment-rest', name: 'apartment-rest')]
+    public function index(): Response
     {
-        $this->apartmentApplicationService = $apartmentApplicationService;
-        $this->queryApartmentRepository = $queryApartmentRepository;
+
+//        $this->apartmentRepository->findById("124123");
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MainCoasd asd ntroller.php',
+        ]);
     }
 
-
-    public function add(ApartmentCreationDto $apartmentDto): void{
-        $this->apartmentApplicationService->addApartment(
-            $apartmentDto->getOwnerId(),
-            $apartmentDto->getStreet(),
-            $apartmentDto->getPostalCode(),
-            $apartmentDto->getHouseNumber(),
-            $apartmentDto->getApartmentNumber(),
-            $apartmentDto->getCity(),
-            $apartmentDto->getCountry(),
-            $apartmentDto->getDescription(),
-            $apartmentDto->getRoomsDefinition(),
-        );
-    }
+//    public function add(ApartmentCreationDto $apartmentDto): void{
+//        $this->apartmentApplicationService->addApartment(
+//            $apartmentDto->getOwnerId(),
+//            $apartmentDto->getStreet(),
+//            $apartmentDto->getPostalCode(),
+//            $apartmentDto->getHouseNumber(),
+//            $apartmentDto->getApartmentNumber(),
+//            $apartmentDto->getCity(),
+//            $apartmentDto->getCountry(),
+//            $apartmentDto->getDescription(),
+//            $apartmentDto->getRoomsDefinition(),
+//        );
+//    }
 
     /// @Route /book/apartmentId
     public function book(string $apartmentId, ApartmentBookingDto $apartmentBookingDto): void{
